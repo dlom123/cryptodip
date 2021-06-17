bfb698c2995a2bd1afd65d405a234ca5e6219d8d
Daniel Lomelino <dlomelino@pm.me>
2021-06-17 11:44:05 -0400

    Allow full precision display of HODLs Adjust data table column widths to reduce horizontal scrollbar scenarios

--- src/utils/functions.js
+++ src/utils/functions.js
@@ -1,8 +1,13 @@
-function getNumDecimals(n, isFlexible=false) {
+function getNumDecimals(n, isFlexible=false, fullPrecision=false) {
+    const nSplit = n.toString().replace(/[$,]/g, '').split(".")
+    if (fullPrecision && nSplit.length > 1) {
+        // return the original full precision decimal length, if requested
+        return nSplit[1].length
+    }
     let numDecimals = 2
     if (typeof n !== 'undefined' && n !== null) {
-        const nSplit = n.toString().replace(/[$,]/g, '').split(".")
         if (n < 1.0) {
+            const nSplit = n.toString().replace(/[$,]/g, '').split(".")
             numDecimals = n < 1.0 ? 4 : 2
             if (isFlexible && nSplit.length > 1) {
                 // extend decimal precision for numbers < 0 that
@@ -20,7 +25,6 @@ function getNumDecimals(n, isFlexible=false) {
 export function formatDollars(n, isFlexible=false) {
     if (typeof n !== 'undefined' && n !== null) {
         const numDecimals = getNumDecimals(n, isFlexible)
-
         return n.toLocaleString('en-US', {
             style: 'currency',
             currency: 'USD',
@@ -30,11 +34,10 @@ export function formatDollars(n, isFlexible=false) {
     return n
 }
 
-export function formatNumber(n, isFlexible=false) {
+export function formatNumber(n, isFlexible=false, fullPrecision=false) {
     if (typeof n !== 'undefined') {
-        const numDecimals = getNumDecimals(n, isFlexible)
         return parseFloat(n).toLocaleString("en-US", {
-            maximumFractionDigits: numDecimals
+            'maximumFractionDigits': getNumDecimals(n, isFlexible, fullPrecision)
         })
     }
     return n
