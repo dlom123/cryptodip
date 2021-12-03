5afd4108f0767644161a766311e188ab68b0bb0a
Daniel Lomelino <dlomelino@pm.me>
2021-12-02 22:37:47 -0500

    Display all-time highs/lows

--- src/views/Dip100.vue
+++ src/views/Dip100.vue
@@ -4,6 +4,22 @@
     <div style="width: 1000px; height: 400px">
       <canvas ref="chart" id="chart"></canvas>
     </div>
+    <div class="mt-2">
+      <v-row no-gutters>
+        <v-col cols="auto" class="px-2 py-1 top50">
+          Top 50 &mdash; high: ${{ alltimeHighs["top"] }}, low: ${{
+            alltimeLows["top"]
+          }}
+        </v-col>
+      </v-row>
+      <v-row no-gutters>
+        <v-col cols="auto" class="px-2 py-1 bottom50">
+          Bottom 50 &mdash; high: ${{ alltimeHighs["bottom"] }}, low: ${{
+            alltimeLows["bottom"]
+          }}
+        </v-col>
+      </v-row>
+    </div>
   </div>
 </template>
 
@@ -15,18 +31,13 @@ export default {
   data() {
     return {
       ctx: null,
-      chartTop100: null,
-    }
-  },
-  computed: {
-    chartDatasets() {
-      return [
+      chartDatasets: [
         {
           label: 'Top 50',
           data: [
             494.96, 486.07, 492.73, 495.17, 505.51,
             502.64, 473.69, 456.90, 484.02, 493.98,
-            495.27, 482.39
+            495.27, 482.39, 481.15
           ],
           backgroundColor: ['blue'],
           borderColor: ['lightblue']
@@ -36,12 +47,27 @@ export default {
           data: [
             493.45, 491.19, 492.53, 497.29, 507.80,
             515.50, 485.90, 463.18, 488.28, 494.20,
-            491.26, 470.36
+            491.26, 470.36, 465.11
           ],
           backgroundColor: ['red'],
           borderColor: ['orange']
         }
-      ]
+      ],
+      chartTop100: null,
+    }
+  },
+  computed: {
+    alltimeHighs() {
+      return {
+        top: Math.max(...this.chartDatasets[0]['data']),
+        bottom: Math.max(...this.chartDatasets[1]['data'])
+      }
+    },
+    alltimeLows() {
+      return {
+        top: Math.min(...this.chartDatasets[0]['data']),
+        bottom: Math.min(...this.chartDatasets[1]['data'])
+      }
     }
   },
   mounted() {
@@ -53,7 +79,7 @@ export default {
         labels: [
           '11/20/21', '11/21/21', '11/22/21', '11/23/21', '11/24/21',
           '11/25/21', '11/26/21', '11/27/21', '11/28/21', '11/29/21',
-          '11/30/21', '12/1/21'
+          '11/30/21', '12/01/21', '12/02/21'
         ],
         datasets: this.chartDatasets
       },
@@ -69,4 +95,13 @@ export default {
 <style scoped lang="sass">
 canvas#chart
   border: 2px solid black
+
+.top50, .bottom50
+  font-weight: bold
+
+.top50
+  background-color: lightblue
+
+.bottom50
+  background-color: orange
 </style>
